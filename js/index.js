7e29a12ed627e22900512a7a0b993c97fd3d5aab
ANSHUMALI SHAW <37397899+ankugolu@users.noreply.github.com>
2021-12-30 11:33:07 +0530

    Added Time Displaying div, modified how the function is called in clock to update div

--- js/index.js
+++ js/index.js
@@ -38,43 +38,144 @@ function clock(){
 }
 setInterval(clock,1000);
 
-function settime(bool){
-    const wakeuptime=document.querySelector("#wakeup-time").value;
-    const lunchtime=document.querySelector("#lunch-time").value;
-    const naptime=document.querySelector("#nap-time").value;
 
+function setDivCall(userValue){
+    console.log("called");
+    let time24=new Date();
+    let h=time24.getHours();
+    let textcontainer=document.querySelector("#text-container");
+    let imgcontainer=document.querySelector("#img-container");
+
+    if(h == userValue.get('naptime')){
+        textcontainer.textContent = "Its Time For Some Sleep !!";
+        imgcontainer.style.backgroundImage="url(./assets/images/goodnight_image.png)";
+    }
+    else if(h == userValue.get('lunchtime')){
+        textcontainer.textContent = "Let's Have Some Lunch !!";
+        imgcontainer.style.backgroundImage="url(./assets/images/lunch_image.png)";
+    }
+    else if(h == userValue.get('wakeuptime')){
+        textcontainer.textContent = "Wake Up !!";
+        imgcontainer.style.backgroundImage="url('./assets/images/wakeup_image.png')";
+    }
+    else{
+        textcontainer.textContent = "Hii All !!";
+        imgcontainer.style.backgroundImage="url(./assets/images/lunch_image.png)";
+    }
+}
+
+async function setDiv(userTimer,userValue,bool){
     // Elements to be Updated
     let highlightdiv=document.querySelector("#highlights-div");
     let textcontainer=document.querySelector("#text-container");
     let imgcontainer=document.querySelector("#img-container");
 
-    highlightdiv.style.display="initial";
+    let hlwt=document.querySelector("#wakeupHL");
+    let hllt=document.querySelector("#lunchHL");
+    let hlnt=document.querySelector("#napHL");
+
+    if(userTimer.get('wakeuptime') === 'Default'){
+        hlwt.style.display = "none";
+    }
+    else{
+        hlwt.style.display = "initial";
+        hlwt.textContent = "Wake Up time:- " + userTimer.get('wakeuptime');
+
+    }
+    if(userTimer.get('lunchtime') === 'Default'){
+        hllt.style.display = "none";
+    }
+    else{
+        hllt.style.display = "initial";
+        hllt.textContent = "Lunch time:- " + userTimer.get('lunchtime');
+
+    }
+    if(userTimer.get('naptime') === 'Default'){
+        hlnt.style.display = "none";
+    }
+    else{
+        hlnt.style.display = "initial";
+        hlnt.textContent = "Nap time:- " + userTimer.get('naptime');
+    }
+
+
+    highlightdiv.style.display="flex";
+
 
     let time24=new Date();
     let h=time24.getHours();
-
-    if(h == wakeuptime){
-        highlightdiv.textContent = "Good Morning !!";
-        textcontainer.textContent = "Wake Up !!";
-        imgcontainer.style.backgroundImage="url('./assets/images/wakeup_image.png')";
-    }
-    else if(h == lunchtime){
-        highlightdiv.textContent = "Good Afternoon !!";
-        textcontainer.textContent = "Let's Have Some Lunch !!";
-        imgcontainer.style.backgroundImage="url(./assets/images/lunch_image.png)";
-    }
-    else if(h == naptime){
-        highlightdiv.textContent = "Good Night !!";
+    if(h == userValue.get('naptime')){
         textcontainer.textContent = "Its Time For Some Sleep !!";
         imgcontainer.style.backgroundImage="url(./assets/images/goodnight_image.png)";
     }
+    else if(h == userValue.get('lunchtime')){
+        textcontainer.textContent = "Let's Have Some Lunch !!";
+        imgcontainer.style.backgroundImage="url(./assets/images/lunch_image.png)";
+    }
+    else if(h == userValue.get('wakeuptime')){
+        textcontainer.textContent = "Wake Up !!";
+        imgcontainer.style.backgroundImage="url('./assets/images/wakeup_image.png')";
+    }
     else{
-        highlightdiv.textContent = "What's Up Fella !!";
         textcontainer.textContent = "Hii All !!";
         imgcontainer.style.backgroundImage="url(./assets/images/lunch_image.png)";
     }
+
+    console.log(userTimer);
+    console.log(userValue);
+
     if(bool === true){
         Swal.fire('Submitted Successfully');
     }
-    setTimeout(settime,1000,false);
+
+    var now = new Date();
+    var delay =  60*60*1000; // 1 hour in msec
+    var start = delay - (now.getMinutes() * 60 + now.getSeconds()) * 1000 + now.getMilliseconds();
+    console.log(start);
+    setTimeout(()=>{
+        setDivCall(userValue);
+        setInterval(()=>setDivCall(userValue),60*60*1000);
+    },start);
 }
+
+
+
+
+function settime(){
+    let userTimer = new Map();
+    let userValue = new Map();
+
+    const naptime=document.querySelector("#nap-time").value;
+    const naptimetext = document.querySelector("#nap-time").options;
+    Array.from(naptimetext).forEach((item)=>{
+        if(item.value === naptime){
+            userTimer.set("naptime",item.textContent);
+            userValue.set("naptime",item.value);
+        }
+    });
+
+
+    const lunchtime=document.querySelector("#lunch-time").value;
+    const lunchtimetext = document.querySelector("#lunch-time").options;
+    Array.from(lunchtimetext).forEach((item)=>{
+        if(item.value === lunchtime){
+            userTimer.set("lunchtime",item.textContent);
+            userValue.set("lunchtime",item.value);
+        }
+    });
+
+    const wakeuptime=document.querySelector("#wakeup-time").value;
+    const wakeuptimeText=document.querySelector("#wakeup-time").options;
+    Array.from(wakeuptimeText).forEach((item)=>{
+        if(item.value === wakeuptime){
+            userTimer.set("wakeuptime",item.textContent);
+            userValue.set("wakeuptime",item.value);
+        }
+    });
+    
+    setDiv(userTimer,userValue,true);
+    
+}
+
+
+
